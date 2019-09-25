ea10f316d43f5e3d3e124e043952a0f2b4331a69
Leonardo Vieira <leovieira20@gmail.com>
2019-09-25 17:50:54 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/dreamsparser/programmable_metronome_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dreamsparser.programmable_metronome_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:programmable_metronome_flutter/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
